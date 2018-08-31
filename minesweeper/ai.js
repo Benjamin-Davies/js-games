000657487cdf2d6c87a7188b6a11f07a1f3c314e
Benjamin Davies <bentendavies@gmail.com>
2018-09-01 09:24:48 +1200

    Add automatic flagging and choosing random empty

--- minesweeper/ai.js
+++ minesweeper/ai.js
@@ -18,31 +18,68 @@ class AI {
     for (let x = 0; x < game.cols; x++) {
       for (let y = 0; y < game.rows; y++) {
         const i = game.index(x, y);
+        const value = game.tiles[i];
 
-if (game.tiles[i]>=0){
-        for (let offX = -1; offX <= 1; offX++) {
-          for (let offY = -1; offY <= 1; offY++) {
-            const newX = x + offX;
-            const newY = y + offY;
-            if (
-              newX >= 0 &&
-              newX < this.cols &&
-              newY >= 0 &&
-              newY < this.rows
-            ) {
-              if (game.tiles[game.index(newX,newY)]===){
+        if (value >= 0) {
+          let neighboors = 0;
 
+          for (let offX = -1; offX <= 1; offX++) {
+            for (let offY = -1; offY <= 1; offY++) {
+              const newX = x + offX;
+              const newY = y + offY;
+              if (
+                newX >= 0 &&
+                newX < game.cols &&
+                newY >= 0 &&
+                newY < game.rows
+              ) {
+                if (game.tiles[game.index(newX, newY)] < 0) {
+                  neighboors++;
+                }
               }
             }
-          }}
+          }
+
+          if (neighboors <= value) {
+            for (let offX = -1; offX <= 1; offX++) {
+              for (let offY = -1; offY <= 1; offY++) {
+                const newX = x + offX;
+                const newY = y + offY;
+                if (
+                  newX >= 0 &&
+                  newX < game.cols &&
+                  newY >= 0 &&
+                  newY < game.rows
+                ) {
+                  if (game.tiles[game.index(newX, newY)] === -1) {
+                    game.toggleFlag(newX, newY);
+                    game.draw();
+                    return;
+                  }
+                }
+              }
+            }
+          }
         }
       }
     }
 
-    game.uncover(
-      Math.floor(game.cols * Math.random()),
-      Math.floor(game.rows * Math.random())
-    );
+    /** @type{[number, number][]} */
+    const empty = [];
+    for (let x = 0; x < game.cols; x++) {
+      for (let y = 0; y < game.rows; y++) {
+        const i = game.index(x, y);
+        const value = game.tiles[i];
+        if (value === -1) {
+          empty.push([x, y]);
+        }
+      }
+    }
+
+    if (empty.length <= 0) return;
+
+    const chosen = empty[Math.floor(empty.length * Math.random())];
+    game.uncover(...chosen);
 
     game.draw();
   }
